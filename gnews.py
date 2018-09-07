c6306473a740fe698daadea2b59af2f08cec2dfc
Roman Dronov <romandronov@gmail.com>
2018-09-08 01:49:42 +1000

    added a news script v0.1

---
+++ gnews.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Google search - canberra times
+Created on Sat Sep  8 00:42:27 2018
+
+@author: Roman
+"""
+
+import urllib.request
+import json
+import re
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+gn_key="8a575cb0ec28485bb090447fcae5a402"
+gn_path="https://newsapi.org/v2/everything?q=flu&sources=google-news-au&apiKey="+gn_key
+
+
+request = urllib.request.Request(gn_path)
+
+
+response = urllib.request.urlopen(request)
+j = json.loads(re.search(r'{.*}',response.read().decode('utf-8')).group())
---
+++ tweetster.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Tweeter searcher
+Created on Sat Sep  8 00:13:23 2018
+@author: Roman
+"""
+import tweepy
+#from twitter_authentification import API_KEY, API_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
+#
+API_KEY= ''
+API_SECRET =''
+ACCESS_TOKEN_KEY = ''
+ACCESS_TOKEN_SECRET= '' 
+#
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+  
+
+auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+query = 'python'
+max_tweets = 100
+searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
