24e3c38e8ea56ae354f57434f096ca63b87171f9
Roman Dronov <romandronov@gmail.com>
2018-09-08 10:45:41 +1000

    going for tweeter vs datasets + vis

--- tweeurl.py
+++ tweeurl.py
@@ -14,20 +14,33 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
-url="https://twitter.com/search?l=&q=flu%20OR%20sick%20OR%20cold%20near%3A%22barton%2C%20%20ACT%22%20within%3A15mi&src=typd&lang=en"
+############################################################################
+
+keywords = ['cancer','flu', 'headache']
+suburbs = ['Adelaide', 'Canberra', 'Brisbane', 'Sydney', 'Perth']
+
+############################################################################
+
 options = Options()
 browser = Browser()
-browser.visit(url)
-time.sleep(2)
-soup = BeautifulSoup(browser.html, 'lxml')
 
-feeds=soup.find('ol', attrs={'class': 'stream-items js-navigable-stream'})
-
-tweets = feeds.find_all('li')
-
-texts= feeds.find_all('p', attrs={'class':'TweetTextSize js-tweet-text tweet-text'})
-
-tt=[]
-for tx in texts:
-    print(tx.text+'\n')
+for keyword in keywords:
+    for sub in suburbs:
+        url="https://twitter.com/search?l=&q="+keyword+"%20near%3A%22"+sub+"%22%20within%3A5mi&src=typd&lang=en"
+        print(url)
+        browser.visit(url)
+        time.sleep(3)
+        soup = BeautifulSoup(browser.html, 'lxml')    
+        feeds=soup.find('ol', attrs={'class': 'stream-items js-navigable-stream'})    
+        try:
+            tweets = feeds.find_all('li')    
+            texts= feeds.find_all('p', attrs={'class':'TweetTextSize js-tweet-text tweet-text'})    
+        except:
+            texts=[' ']
+        tt=[]
+        with open("data/"+keyword+"_"+sub+".txt", "a") as myfile:
+            for tx in texts:
+                print(tx.text+'\n')
+                tt.append(tx.text)
+                myfile.write(tx.text)
 
